48e5727c44e3e8526ba2b8cee98ef523f43d7917
Caroline <41442206+kapiaszczyk@users.noreply.github.com>
2021-09-16 11:16:39 +0200

    Create sumDouble.java

---
+++ Coding Bat Solutions/Warmup 1/sumDouble.java
@@ -0,0 +1,6 @@
+// Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+public int sumDouble(int a, int b) {
+    if (a == b) return a * 4;
+    return a + b;
+}
