3d6c6dd2bf26b9d7c6f0fc4207e57a6bfe1c060b
Caroline <41442206+kapiaszczyk@users.noreply.github.com>
2021-09-16 10:51:53 +0200

    Update frontBack.java

--- Coding Bat Solutions/Warmup 1/frontBack.java
+++ Coding Bat Solutions/Warmup 1/frontBack.java
@@ -1,12 +1,12 @@
-// Warmup 1 - frontBack
 // Given a string, return a new string where the first and last chars have been exchanged.
 
 public String frontBack(String str) {
-    if (str.length() <= 1) { 
-      return str;
+    if (str.length() > 1) {
+        int last = str.length();
+        String  End = str.substring(last-1, last);
+        String  Start = str.substring(0,1);
+        String Middle = str.substring(1, last-1);
+        return (End + Middle + Start);
     }
-    else {
-      String mid = str.substring(1, str.length()-1);
-      return str.charAt(str.length()-1) + mid + str.charAt(0);
-    }
+    else return (str);
 }
