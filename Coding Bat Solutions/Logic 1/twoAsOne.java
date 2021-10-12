fe2ef60daf0def4b117db7c58094d4af217393c8
Caroline <41442206+kapiaszczyk@users.noreply.github.com>
2021-10-12 10:03:40 +0200

    Create twoAsOne.java

---
+++ Coding Bat Solutions/Logic 1/twoAsOne.java
@@ -0,0 +1,8 @@
+// Given three ints, a b c, return true if it is possible to add two of the ints to get the third.
+
+public boolean twoAsOne(int a, int b, int c) {
+  if ((a + b) == c || (b + c) == a || (a + c) == b) {
+    return true;
+  }
+  return false;
+}
