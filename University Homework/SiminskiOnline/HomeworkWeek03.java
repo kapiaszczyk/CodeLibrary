2705e80c1cfbeb742c35bf5922e6574e96d41b0c
Caroline <41442206+kapiaszczyk@users.noreply.github.com>
2021-11-16 19:38:50 +0100

    Update HomeworkWeek03.java

--- University Homework/SiminskiOnline/HomeworkWeek03.java
+++ University Homework/SiminskiOnline/HomeworkWeek03.java
@@ -88,3 +88,69 @@ public class Main {
 }
 
 // 2.6
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+	static void calculateAverage() {
+		System.out.println("I just got executed!");
+	}
+
+	public static void main(String[] args) {
+
+		// Variables and constants
+		final int HOW_MANY_DAYS = 7;
+
+		double HighestPossible = Double.MIN_VALUE;
+		double LowestPossible = Double.MAX_VALUE;
+		double averageTemperature = 0;
+
+		int dayOfHighest = 0, dayOfLowest = 0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		// Create array called daysArray that stores "Day" objects
+		Day[] daysArray = new Day[HOW_MANY_DAYS];
+
+		// Main logic
+		for (int i = 0; i <= (HOW_MANY_DAYS - 1); i++) {
+
+			// create a Day object
+			daysArray[i] = new Day();
+			System.out.print("Set day number " + (i + 1) + " temperature: ");
+			daysArray[i].temperature = scanner.nextDouble();
+			
+			// Set the lowest temperature
+			if (daysArray[i].temperature >= 0 && daysArray[i].temperature < LowestPossible) {
+				LowestPossible = daysArray[i].temperature;
+				dayOfLowest = i;
+			}
+			
+			// Set the highest temperature
+			else if (daysArray[i].temperature < 0 && daysArray[i].temperature > HighestPossible) {
+				HighestPossible = daysArray[i].temperature;
+				dayOfHighest = i;
+			}
+
+
+		}
+
+		// Calculate the Average (could have i been a method?)
+		for (int i = 0; i <= (HOW_MANY_DAYS - 1); i++) {
+			// sum up the days
+			averageTemperature = averageTemperature + daysArray[i].temperature;
+			// in the last loop, divide the sum by the number of days
+			if (i == 6) averageTemperature = averageTemperature / HOW_MANY_DAYS;
+		}
+
+
+		// Output for the user
+		System.out.println("On day " + (dayOfLowest + 1) + " there was lowest positive temperature, reading " + daysArray[dayOfLowest].temperature);
+		System.out.println("On day " + (dayOfHighest + 1) + " there was highest negative temperature, reading " + daysArray[dayOfHighest].temperature);
+		System.out.println("The average temperature is " + averageTemperature);
+
+
+	}
+
+}
