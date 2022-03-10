d4ce9e94f154dea50cb7b72919f4be2d266747d9
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-03-10 12:18:07 +0100

    Add solved problem

--- University Homework/Zadanie_02.cpp
+++ University Homework/Zadanie_02.cpp
@@ -26,3 +26,41 @@ int main() {
 }
 
 // Zadanie 2.2
+
+#include <iostream>
+
+
+int main() {
+
+	int n = 0; 
+	std::cout << "Podaj liczbe" << std::endl;
+	std::cin >> n;
+	int min = n;
+	int max = n;
+	int suma = n;
+
+	while (n != 1) {
+		if (n % 2 == 0) { 
+			n = n / 2; 
+			std:: cout << n << std::endl;
+			if (n > max) max = n;
+			if (n < min) min = n;
+			suma = suma + n;
+			
+		}
+		else {
+			n = 3 * n + 1;
+			std::cout << n << std::endl;
+			if (n > max) max = n;
+			if (n < min) min = n;
+			suma = suma + n;
+
+		}
+	}
+
+	std::cout << max << std::endl;
+	std::cout << min << std::endl;
+	std::cout << suma << std::endl;
+
+	return 0;
+}
