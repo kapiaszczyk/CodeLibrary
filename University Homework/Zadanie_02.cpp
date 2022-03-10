277b26257848b8db408b64f1c252e14c49ce672b
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-03-10 12:27:19 +0100

    Add solved problem

--- University Homework/Zadanie_02.cpp
+++ University Homework/Zadanie_02.cpp
@@ -64,3 +64,56 @@ int main() {
 
 	return 0;
 }
+
+// Zadanie 2.3
+
+ #include <iostream>
+
+ void calculator() {
+
+     double pierwszaLiczba = 0, drugaLiczba = 0, wynik = 0;
+     char operatorArytmetyczny = '0';
+     char flag = 'y';
+
+     while (flag == 'y') {
+
+         std::cout << "Podaj liczbe 1: " << std::endl;
+         std::cin >> pierwszaLiczba;
+         std::cout << "Podaj operator" << std::endl;
+         std::cin >> operatorArytmetyczny;
+         std::cout << "Podaj liczbe 2: " << std::endl;
+         std::cin >> drugaLiczba;
+
+         switch (operatorArytmetyczny) {
+         case '+':
+             wynik = pierwszaLiczba + drugaLiczba;
+             std::cout << "Wynik: " << wynik << std::endl;
+             break;
+         case '-':
+             wynik = pierwszaLiczba - drugaLiczba;
+             std::cout << "Wynik: " << wynik << std::endl;
+             break;
+         case '*':
+             wynik = pierwszaLiczba * drugaLiczba;
+             std::cout << "Wynik: " << wynik << std::endl;
+             break;
+         case '/':
+             if (drugaLiczba == 0) std::cout << "Nie mozna dzielic przez 0" << std::endl;
+             wynik = pierwszaLiczba * drugaLiczba;
+             std::cout << "Wynik: " << wynik << std::endl;
+             break;
+         default:
+             std::cout << "Blad" << std::endl;
+         }
+
+         std::cout << "Czy chcesz kontynuowac? y/n" << std::endl; 
+         std::cin >> flag; 
+     }
+
+ }
+
+ int main() {
+     calculator();
+     return 0;
+ }
+
