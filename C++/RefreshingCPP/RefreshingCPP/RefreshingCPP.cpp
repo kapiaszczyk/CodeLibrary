51d3e4ff611d1172d4a9fabb63cd9798ab65e8fc
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-03-14 17:42:27 +0100

    Modify file

--- C++/RefreshingCPP/RefreshingCPP/RefreshingCPP.cpp
+++ C++/RefreshingCPP/RefreshingCPP/RefreshingCPP.cpp
@@ -1,95 +1,39 @@
 ﻿// Zadanie 3.1
 
 #include <iostream>
-#include <random>
 
-void fillArray(double *array, int arraySize) {
-
-	// int arraySize = sizeof(*array) / sizeof(double) - 1; // doesnt work, as we are calculating size of the pointer ; it is not possible to get the number of elements anymore ; instead use struct or pass the leght
-
-	const double rangeFrom = 3.0;
-	const double rangeTo = 9.0;
-	std::random_device						rand_dev;							// uniformly-distributed integer random number generator that produces non-deterministic random numbers
-	std::mt19937							generator(rand_dev());				// A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits
-	std::uniform_real_distribution<double>	distr(rangeFrom, rangeTo);			// Produces random floating-point values 
-	
-	for (int i = 0; i < arraySize; i++) {
-		double randomNumber = distr(generator);
-		double rounded = std::round(10 * randomNumber) / 10;					// Rounding up to one decimal point
-		array[i] = rounded;
-	}
-}
-
-void displayArray(double* array, int arraySize) {
-
-	std::cout << "Array:" << std::endl;
-
-	for (int i = 0; i < arraySize; i++) {
-		std::cout << array[i] << "   ";
-	}
-
-	std::cout << std::endl;
-}
-
-void displayArrayInverted(double* array, int arraySize) {
-
-	std::cout << "Inverted array:" << std::endl;
-
-	for (int i = arraySize - 1; i >= 0; i--) {
-		std::cout << array[i] << "   ";
-	}
-
-	std::cout << std::endl;
-}
-
-double average(double* array, int arraySize, bool flag) {
-
-	if (flag == true) std::cout << "Average: ";
-
-	double sum = 0; 
-
-	for (int i = 0; i < arraySize; i++) {
-		sum = sum + array[i];
-	}
-
-	return std::round(10 * (sum / arraySize)) / 10; ;
-
-	// std::cout << (sum / arraySize) << std::endl;
-
-}
-
-void biggerLesser(double* array, int arraySize, double average) {
-
-	int bigger = 0, lesser = 0;
-
-	for (int i = 0; i < arraySize; i++) {
-		if (array[i] > average) {
-			bigger++;
-		}
-		else {
-			lesser++;
-		}
-	}
-
-	std::cout << "Bigger: " << bigger << std::endl;
-	std::cout << "Lesser: " << lesser << std::endl;
-
-}
 
 int main() {
 
-	const int ARRAY_LENGHT = 100;
-	double array[ARRAY_LENGHT];
-	bool flag = true;
+	int decimalNumber = 0;
 
+	std::cout << "Your input: ";
+	std::cin >> decimalNumber;
+	int decimalNumberUnmodified = decimalNumber;
 
-	fillArray(array, ARRAY_LENGHT);
-	displayArray(array, ARRAY_LENGHT);
-	displayArrayInverted(array, ARRAY_LENGHT);
-	std::cout << average(array, ARRAY_LENGHT, true) << std::endl;
-	biggerLesser(array, ARRAY_LENGHT, average(array, ARRAY_LENGHT, false)); 
+	int array[32];
+
+	int i = 0;
+
+	for (i = 0; decimalNumber > 0; i++) {
+		array[i] = decimalNumber % 2;
+		decimalNumber = decimalNumber / 2;
+	}
+
+	std::cout << "Your output in binary: ";
+
+	if (decimalNumberUnmodified > 0) {
+		std::cout << "0.";
+	}
+	else {
+		std::cout << "1.";
+	}
+	for (i = i - 1; i >= 0; i--) {
+		std::cout << array[i];
+	}
 
 	return 0;
+
 }
 
 
@@ -129,5 +73,41 @@ Numeric random(Numeric from, Numeric to)
 
 	return dist(gen, typename dist_type::param_type{ from, to });
 }
+
+*/
+
+/*
+
+
+	int decimalNumber = 0;
+
+	std::cout << "Your input: ";
+	std::cin >> decimalNumber;
+	int decimalNumberUnmodified = decimalNumber;
+
+	int array[32];
+
+	int i = 0;
+
+	convertToBinary(decimalNumber, array, i);
+
+	for (i = 0; decimalNumber > 0; i++) {
+		array[i] = decimalNumber % 2;
+		decimalNumber = decimalNumber / 2;
+	}
+
+	std::cout << "Your output in binary: ";
+
+	if (decimalNumberUnmodified > 0) {
+		std::cout << "0.";
+	}
+	else {
+		std::cout << "1.";
+	}
+	for (i = i - 1; i >= 0; i--) {
+		std::cout << array[i];
+	}
+
+	return 0;
 
 */
