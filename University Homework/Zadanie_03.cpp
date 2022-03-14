20e5235aa3d141b73be88b2348b1c6260cef57b8
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-03-14 20:13:47 +0100

    Modify file

--- University Homework/Zadanie_03.cpp
+++ University Homework/Zadanie_03.cpp
@@ -127,3 +127,83 @@ int main() {
 	return 0;
 
 }
+
+// Zadanie 3.3
+
+#include <iostream>
+#include <random> // https://en.cppreference.com/w/cpp/numeric/random
+
+int main() {
+
+	// Initalise variables and constants
+	int const ARRAY_DIMENSION = 10;
+	int const GENERATE_RANGE = 100;
+	int maxOne = -1 * (GENERATE_RANGE);
+	int maxTwo = -1 * (GENERATE_RANGE);
+	int array[ARRAY_DIMENSION][ARRAY_DIMENSION];
+
+	// Define generating function parameters
+	const int range_from = -1 * (GENERATE_RANGE);							// Define scope
+	const int range_to = GENERATE_RANGE;									// ... 
+	std::random_device                  rand_dev;							// Uniformly-distributed integer random number generator ?
+	std::knuth_b                        generator(rand_dev());				// Choose algorythm
+	std::uniform_int_distribution<int>  distr(range_from, range_to);		// Produces integer values evenly distributed across a range ?
+
+	// Generate random numbers for the array
+	for (int i = 0; i < ARRAY_DIMENSION; i++) {
+		for (int j = 0; j < ARRAY_DIMENSION; j++) {
+			array[i][j] = distr(generator);
+		}
+	}
+
+	// Print out the array 
+	for (int i = 0; i < ARRAY_DIMENSION; i++) {
+		for (int j = 0; j < ARRAY_DIMENSION; j++) {
+			// std::cout << array[i][j] << "   ";
+			printf("%5d", array[i][j]);
+		}
+		std::cout << std::endl;
+	}
+
+	// Initalise array to store min values for each column 
+	int min[ARRAY_DIMENSION];
+	for (int i = 0; i < ARRAY_DIMENSION; i++) {
+		min[i] = 0;
+	}
+
+	// Calculate minimum value in each respective column
+	std::cout << std::endl;
+	std::cout << "Min for respective columns: " << std::endl;
+
+	for (int j = 0; j < ARRAY_DIMENSION; j++) {
+		for (int i = 0; i < ARRAY_DIMENSION; i++) {
+			if (array[i][j] < min[j]) {
+				min[j] = array[i][j];
+			}
+		}
+		printf("%5d", min[j]);
+	}
+
+	// Calculate diagonal max
+	std::cout << std::endl;
+	std::cout << std::endl;
+	for (int j = 0; j < ARRAY_DIMENSION; j++) {
+		if (array[j][j] > maxOne) {
+			maxOne = array[j][j];
+		}
+	}
+	std::cout << "Max one: " << maxOne << std::endl;
+	std::cout << std::endl;
+
+	for (int j = ARRAY_DIMENSION - 1; j > 0; j--) {
+		if (array[j][j] > maxTwo) {
+			maxTwo = array[j][j];
+		}
+	}
+	std::cout << "Max two: " << maxTwo << std::endl;
+	std::cout << std::endl;
+
+	return 0;
+	// std::exit(EXIT_SUCCESS);
+	
+}
