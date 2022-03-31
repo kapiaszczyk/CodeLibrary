49f6eac61bf8ea74974ef2631533686d39596f79
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-03-31 12:24:33 +0200

    Add solution to 4.2

--- University Homework/Zadanie 04.cpp
+++ University Homework/Zadanie 04.cpp
@@ -1,6 +1,7 @@
+// Zadanie 4.1
+
 #include <iostream>
 #include <cstring>
-
 
 int count(const char str[], char letterToFind, int &firstOccurance, int &lastOccurance, char &letterToFindConsole) {
 	int counter = 0;
@@ -32,5 +33,37 @@ int main() {
 	std::cout << count(str, 'c', firstOccurance, lastOccurance, letterToFindConsole) << std::endl;
 	std::cout << "First occurance of " << letterToFindConsole << " is at position nr " << firstOccurance << std::endl;
 	std::cout << "Last occurance of " << letterToFindConsole << " is at position nr " << lastOccurance << std::endl;
+
+}
+
+// Zadanie 4.2
+
+#include <iostream>
+#include <cstring>
+
+void print(int array[], int size, const char separator[] = " ") {
+
+	int separatorLenght = strlen(separator);
+
+	for (int i = 0; i < size; i++) {
+		std::cout << array[i]; 
+		for (int i = 0; i < separatorLenght; i++) {
+			std::cout << separator[i];
+		}
+	}
+}
+
+int main() {
+
+	const int N = 5;
+	int punkty[N] = { 12, 5, 0, 20, 10 };
+
+	std::cout << "punkty: ";
+	print(punkty, N);
+
+	std::cout << std::endl;
+
+	std::cout << "punkty: ";
+	print(punkty, N, ", ");
 
 }
