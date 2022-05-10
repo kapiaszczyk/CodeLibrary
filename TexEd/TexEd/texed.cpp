b35a1b66ca62b71e2af75ca12f26f841ba7934f4
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-05-10 20:21:46 +0200

    Add TexEd files

---
+++ TexEd/TexEd/texed.cpp
@@ -0,0 +1,13 @@
+// TexEd.cpp
+#include <cstdlib>
+#include <iostream>
+#include <windows.h>
+
+int main() {
+
+    std::cout << "I am in fact executing" << std::endl;
+
+    Sleep(360);
+    return EXIT_SUCCESS;
+
+}
