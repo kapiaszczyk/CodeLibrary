2af9feead476d06692a819b0633b47ec1ca9ab0c
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-05-10 20:23:16 +0200

    File cleanup

--- TexEd/TexEd/texed.cpp
+++ TexEd/TexEd/texed.cpp
@@ -1,13 +1,10 @@
 // TexEd.cpp
 #include <cstdlib>
 #include <iostream>
-#include <windows.h>
 
 int main() {
 
     std::cout << "I am in fact executing" << std::endl;
-
-    Sleep(360);
     return EXIT_SUCCESS;
 
 }
